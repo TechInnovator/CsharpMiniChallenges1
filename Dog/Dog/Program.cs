ca8e27b88ca4de28bdea023c7dd44124c0407656
techinnovator <techinnovator01@gmail.com>
2018-07-06 10:21:04 -0700

    Added code for Dog

--- Dog/Dog/Program.cs
+++ Dog/Dog/Program.cs
@@ -6,7 +6,13 @@ namespace Dog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.bark(3);
+            Console.WriteLine(puppy.getTag());
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.bark(1);
+            Console.WriteLine(myDog.getTag());
         }
     }
 }
